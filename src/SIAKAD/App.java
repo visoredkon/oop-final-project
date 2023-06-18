d04df7f0cfd09ccd0af453b22d1c06cb02021623
Pahril <88573655+visoredkon@users.noreply.github.com>
2023-06-19 00:58:01 +0800

    refactor: Memperbarui class App untuk menjalankan GUI

--- src/SIAKAD/App.java
+++ src/SIAKAD/App.java
@@ -1,7 +1,26 @@
 package SIAKAD;
 
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage
+                .getIcons()
+                .add(new Image(App.class.getResourceAsStream("resources/logoSIAKAD.png")));
+
+        Parent root = FXMLLoader.load(getClass().getResource("views/LoginView.fxml"));
+        primaryStage.setTitle("Login");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
