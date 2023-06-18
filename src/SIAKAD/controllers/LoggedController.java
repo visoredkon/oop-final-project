5f07a726e34f656e8477201152ff8d57f1a5c9af
Miyawn <tengku.rayhan26@gmail.com>
2023-06-19 01:26:48 +0800

    feat: Membuat LoggedController

---
+++ src/SIAKAD/controllers/LoggedController.java
@@ -0,0 +1,34 @@
+package SIAKAD.controllers;
+
+import SIAKAD.constants.Role;
+import SIAKAD.models.Dosen;
+import SIAKAD.models.LoginData;
+import SIAKAD.models.Mahasiswa;
+
+public class LoggedController {
+    private static String username;
+
+    public LoggedController(String username) {
+        LoggedController.username = username;
+    }
+
+    public static String getUsername() {
+        return username;
+    }
+
+    public static boolean isAdmin() {
+        LoginData loginData = LoginData.getLoginData(username);
+        if (loginData.getRole() == Role.ADMIN) {
+            return true;
+        }
+        return false;
+    }
+
+    public static Dosen getDosen() {
+        return Dosen.retrieveFromDatabase(username);
+    }
+
+    public static Mahasiswa getMahasiswa() {
+        return Mahasiswa.retrieveFromDatabase(username);
+    }
+}
