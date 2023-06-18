d9eff90b76a03236fdbea00a766b0186040afb7b
Miyawn <tengku.rayhan26@gmail.com>
2023-06-19 05:08:30 +0800

    feat: Membuat controller sidebar mahasiswa

--- src/SIAKAD/controllers/MahasiswaGUIController.java
+++ src/SIAKAD/controllers/MahasiswaGUIController.java
@@ -1,5 +1,58 @@
 package SIAKAD.controllers;
 
+import SIAKAD.models.Mahasiswa;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+
 public class MahasiswaGUIController {
-    public void setUsernameText(String username) {}
+    @FXML private Text usernameText;
+    @FXML private Text namaText;
+    @FXML private Button dashboardButton;
+    @FXML private Button dosenButton;
+    @FXML private Button mahasiswaButton;
+    @FXML private Button mataKuliahButton;
+    @FXML private Button mataKuliahNilaiButton;
+    @FXML private Button logoutButton;
+    @FXML private StackPane contentStackPane;
+
+    @FXML
+    private void initialize() {
+        PageGUIController controller = new PageGUIController();
+
+        dashboardButton.setOnAction(
+                event ->
+                        controller.showSidebarContent(
+                                "Dashboard", contentStackPane, "MahasiswaGUI"));
+        dosenButton.setOnAction(
+                event -> controller.showSidebarContent("Dosen", contentStackPane, "MahasiswaGUI"));
+        mahasiswaButton.setOnAction(
+                event ->
+                        controller.showSidebarContent(
+                                "Mahasiswa", contentStackPane, "MahasiswaGUI"));
+        mataKuliahButton.setOnAction(
+                event ->
+                        controller.showSidebarContent(
+                                "MataKuliah", contentStackPane, "MahasiswaGUI"));
+        mataKuliahNilaiButton.setOnAction(
+                event ->
+                        controller.showSidebarContent(
+                                "NilaiMataKuliah", contentStackPane, "MahasiswaGUI"));
+        logoutButton.setOnAction(event -> controller.logout(logoutButton));
+    }
+
+    public void setUsernameText(String username) {
+        new LoggedController(username);
+
+        usernameText.setText(username);
+        retrieveMahasiswa();
+    }
+
+    private void retrieveMahasiswa() {
+        String username = usernameText.getText();
+        Mahasiswa mahasiswa = Mahasiswa.retrieveFromDatabase(username);
+        namaText.setText(mahasiswa.getName());
+    }
 }
---
+++ src/SIAKAD/controllers/SidebarMahasiswa/NilaiMataKuliahController.java
@@ -0,0 +1,71 @@
+package SIAKAD.controllers.SidebarMahasiswa;
+
+import SIAKAD.controllers.LoggedController;
+import SIAKAD.models.MataKuliah;
+import SIAKAD.models.NilaiMataKuliah;
+import SIAKAD.utils.DB;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.text.Text;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class NilaiMataKuliahController {
+    @FXML private Text ipkText;
+    @FXML private TableView<NilaiMataKuliah> nilaiMataKuliahTableView;
+    @FXML private TableColumn<NilaiMataKuliah, String> kodeColumn;
+    @FXML private TableColumn<NilaiMataKuliah, String> namaMataKuliahColumn;
+    @FXML private TableColumn<NilaiMataKuliah, String> nilaiColumn;
+
+    private final ObservableList<NilaiMataKuliah> dataNilaiMataKuliah =
+            FXCollections.observableArrayList();
+
+    @FXML
+    private void initialize() {
+        ipkText.setText("IPK: " + LoggedController.getMahasiswa().getIpk());
+
+        kodeColumn.setCellValueFactory(new PropertyValueFactory<>("kodeMataKuliah"));
+        namaMataKuliahColumn.setCellValueFactory(new PropertyValueFactory<>("namaMataKuliah"));
+        namaMataKuliahColumn.setCellValueFactory(
+                cellData -> {
+                    StringProperty property = new SimpleStringProperty();
+                    NilaiMataKuliah nilaiMataKuliah = cellData.getValue();
+                    property.setValue(nilaiMataKuliah.getNamaMataKuliah());
+                    return property;
+                });
+        nilaiColumn.setCellValueFactory(new PropertyValueFactory<>("nilai"));
+
+        nilaiMataKuliahTableView.setColumnResizePolicy(
+                TableView.CONSTRAINED_RESIZE_POLICY_ALL_COLUMNS);
+        loadData();
+    }
+
+    private void loadData() {
+        nilaiMataKuliahTableView.setItems(dataNilaiMataKuliah);
+        dataNilaiMataKuliah.clear();
+        String tableName = "nilai_mata_kuliah";
+        String condition = "mahasiswa_nim = '" + LoggedController.getMahasiswa().getNim() + "'";
+        ResultSet resultSet = DB.selectAll(tableName, condition);
+
+        try {
+            while (resultSet.next()) {
+                NilaiMataKuliah nilaiMataKuliah = new NilaiMataKuliah();
+                nilaiMataKuliah.setMataKuliah(
+                        MataKuliah.retrieveFromDatabase(resultSet.getString("kode_mata_kuliah")));
+                nilaiMataKuliah.setNilai(resultSet.getDouble("nilai"));
+
+                dataNilaiMataKuliah.add(nilaiMataKuliah);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
