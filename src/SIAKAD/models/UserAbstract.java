c17a9a15653133298483ee99ff1007fc90491b30
Pahril <88573655+visoredkon@users.noreply.github.com>
2023-06-18 11:08:22 +0800

    feat: Membuat class abstract

---
+++ src/SIAKAD/models/UserAbstract.java
@@ -0,0 +1,52 @@
+package SIAKAD.models;
+
+import SIAKAD.constants.Sex;
+import SIAKAD.interfaces.User;
+
+public abstract class UserAbstract implements User {
+    private String password;
+    private String name;
+    private Sex sex;
+    private String phone;
+    private String address;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Sex getSex() {
+        return sex;
+    }
+
+    public void setSex(Sex sex) {
+        this.sex = sex;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
